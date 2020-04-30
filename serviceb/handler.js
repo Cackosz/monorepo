3d94240a7d929044638321b5e31966b037491bd2
Marco Ramirez Davila <mramirezd@visionconsulting.com.mx>
2020-04-30 17:00:29 -0500

    Se agrega nuevo servicio

--- serviceb/handler.js
+++ serviceb/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'MONOREPOOSS!',
+        message: 'MONOREPOOSSss!',
         input: event,
       },
       null,
